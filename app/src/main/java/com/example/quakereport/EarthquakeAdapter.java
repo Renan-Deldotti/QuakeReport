d8839f39d5f32ce624a825f652ef86a4d0d9d36b
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-03-23 21:45:34 -0300

    Update EarthquakeAdapter.java

--- app/src/main/java/com/example/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/quakereport/EarthquakeAdapter.java
@@ -10,11 +10,15 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 
 public class EarthquakeAdapter extends ArrayAdapter<Earthquakes> {
+
+    private static final String LOCATION_SEPARATOR = " of ";
 
     public EarthquakeAdapter(Context context, ArrayList<Earthquakes> arrayList){
         super(context,0,arrayList);
@@ -40,14 +44,23 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquakes> {
         // Cria a variavel para receber os TextViews
         TextView useTextView;
         // Recebe o valor da magnitude e seta no TextView
+        DecimalFormat decimalFormat = new DecimalFormat("0.0");
         useTextView = listItemView.findViewById(R.id.magnitude);
-        useTextView.setText(""+currentEarthquakes.getMagnitude());
+        useTextView.setText(""+decimalFormat.format(currentEarthquakes.getMagnitude()));
+        //Divide a String de getLocation
+        String[] textToShow;
+        if (currentEarthquakes.getLocation().contains(LOCATION_SEPARATOR)){
+            textToShow = currentEarthquakes.getLocation().split(LOCATION_SEPARATOR);
+            textToShow[0] += LOCATION_SEPARATOR;
+        }else {
+            textToShow = new String[]{getContext().getString(R.string.near_the), currentEarthquakes.getLocation()};
+        }
         // Seta a proximidade
         useTextView =  listItemView.findViewById(R.id.nearby);
-        useTextView.setText(currentEarthquakes.getLocation());
+        useTextView.setText(textToShow[0]);
         // Seta o nome da cidade
         useTextView =  listItemView.findViewById(R.id.cityName);
-        useTextView.setText(currentEarthquakes.getLocation());
+        useTextView.setText(textToShow[1]);
         // Seta a data
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
         String formattedDate =  simpleDateFormat.format(new Date(currentEarthquakes.getDate()));
