fa4a117a4bccfdf5994265792f042473e7f4a72d
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-03-28 00:40:54 -0300

    Loader update
    Changed to Loader

--- app/src/main/java/com/example/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/quakereport/EarthquakeActivity.java
@@ -1,12 +1,18 @@
 package com.example.quakereport;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.LoaderManager;
 import android.content.Intent;
+import android.content.Loader;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -15,18 +21,63 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class EarthquakeActivity extends AppCompatActivity {
+public class EarthquakeActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Earthquakes>> {
 
+    EarthquakeAdapter earthquakeAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
-        String urlString = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&limit=100&minmagnitude=2&orderby=time";
-        EarthquakeAsyncTask earthquakeAsyncTask = new EarthquakeAsyncTask();
-        earthquakeAsyncTask.execute(urlString);
+        earthquakeAdapter = new EarthquakeAdapter(this,new ArrayList<Earthquakes>());
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(earthquakeAdapter);
+        getLoaderManager().initLoader(1,null, this).forceLoad();
+        //EarthquakeAsyncTask earthquakeAsyncTask = new EarthquakeAsyncTask();
+        //earthquakeAsyncTask.execute(urlString);
     }
 
-    private class EarthquakeAsyncTask extends AsyncTask<String,Void, List<Earthquakes>>{
+    @Override
+    public Loader<List<Earthquakes>> onCreateLoader(int id, Bundle args) {
+        Log.e("Loader","Loader onCreate was called.");
+        return new EarthquakeLoader(EarthquakeActivity.this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Earthquakes>> loader, List<Earthquakes> data) {
+        Log.e("Loader","Loader onLoadFinished was called.");
+        updateUi(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Earthquakes>> loader) {
+        Log.e("Loader","Loader onLoaderReset was called.");
+        earthquakeAdapter = new EarthquakeAdapter(EarthquakeActivity.this,new ArrayList<Earthquakes>());
+    }
+
+    protected void updateUi(List<Earthquakes> earthquakes) {
+        // Pega a referencia ao ListView "list"
+        ListView earthquakeListView = findViewById(R.id.list);
+        // Cria o adapter para o ArrayList Earthquake
+        ArrayList<Earthquakes> earthquakesArrayList = (ArrayList<Earthquakes>) earthquakes;
+        final EarthquakeAdapter adapter = new EarthquakeAdapter(EarthquakeActivity.this,earthquakesArrayList);
+        // Adiciona o Adapter ao ListView
+        earthquakeListView.setAdapter(adapter);
+        // Adiciona o ClickListener na lista
+        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(EarthquakeActivity.this,"Abrindo a noticia no navegador...",Toast.LENGTH_LONG).show();
+                Earthquakes currentEarthquakes = adapter.getItem(position);
+//                Uri uri = Uri.parse(currentEarthquakes.getUrl());
+//                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+//                startActivity(intent);
+                // Jeito mais pratico
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(currentEarthquakes.getUrl())));
+            }
+        });
+    }
+
+    /*private class EarthquakeAsyncTask extends AsyncTask<String,Void, List<Earthquakes>>{
 
         @Override
         protected List<Earthquakes> doInBackground(String... strings) {
@@ -54,13 +105,13 @@ public class EarthquakeActivity extends AppCompatActivity {
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                     Toast.makeText(EarthquakeActivity.this,"Abrindo a noticia no navegador...",Toast.LENGTH_LONG).show();
                     Earthquakes currentEarthquakes = adapter.getItem(position);
-                /*Uri uri = Uri.parse(currentEarthquakes.getUrl());
-                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
-                startActivity(intent);*/
+//                Uri uri = Uri.parse(currentEarthquakes.getUrl());
+//                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+//                startActivity(intent);
                     // Jeito mais pratico
                     startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(currentEarthquakes.getUrl())));
                 }
             });
         }
-    }
+    }*/
 }
--- app/src/main/java/com/example/quakereport/EarthquakeLoader.java
+++ app/src/main/java/com/example/quakereport/EarthquakeLoader.java
@@ -1,15 +1,17 @@
 package com.example.quakereport;
 
+import android.content.AsyncTaskLoader;
 import android.content.Context;
+import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.loader.content.AsyncTaskLoader;
 
 import java.util.List;
 
 class EarthquakeLoader extends AsyncTaskLoader<List<Earthquakes>> {
 
+    final String urlString = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&limit=100&minmagnitude=2&orderby=time";
     public EarthquakeLoader(@NonNull Context context) {
         super(context);
     }
@@ -17,6 +19,12 @@ class EarthquakeLoader extends AsyncTaskLoader<List<Earthquakes>> {
     @Nullable
     @Override
     public List<Earthquakes> loadInBackground() {
-        return null;
+        // Checa se existe uma String para ser convertida em URL
+        if (TextUtils.isEmpty(urlString)){
+            return null;
+        }
+        // Chama o metodo para pegar o JSON da url
+        List<Earthquakes> earthquakes = QueryUtils.extractEarthquakes(urlString);
+        return earthquakes;
     }
 }
