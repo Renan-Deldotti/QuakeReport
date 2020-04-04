50dfbb2704ffe06084b251fdbc738a01a41e9f98
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-04-04 18:24:19 -0300

    Show pref to user

--- app/src/main/java/com/example/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/quakereport/SettingsActivity.java
@@ -1,7 +1,13 @@
 package com.example.quakereport;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +18,28 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
     }
-    public static class EarthquakePreferenceFragment extends PreferenceFragment{
+    public static class EarthquakePreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+            Preference minMag = findPreference(getString(R.string.settings_min_magnitude_key));
+            bindPreferenceSummaryToValue(minMag);
+        }
+
+        private void bindPreferenceSummaryToValue(Preference minMag) {
+            minMag.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(minMag.getContext());
+            String prefString = preferences.getString(minMag.getKey(),"");
+            onPreferenceChange(minMag,prefString);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            //Toast.makeText(this, "Preference " + preference.toString() + "changed", Toast.LENGTH_LONG);
+            preference.setSummary(newValue.toString());
+            return true;
         }
     }
 }
