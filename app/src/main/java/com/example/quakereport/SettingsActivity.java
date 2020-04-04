ee2a392d9a2578b01b4b7eb8f3fe61a1eae945cd
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-04-04 00:52:11 -0300

    Create SettingsActivity.java

---
+++ app/src/main/java/com/example/quakereport/SettingsActivity.java
@@ -0,0 +1,17 @@
+package com.example.quakereport;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    public static class EarthquakePreferenceFragment extends PreferenceFragment{
+        
+    }
+}
